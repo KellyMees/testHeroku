540db46fbd6de1b7f783341749a6c4e72f78fd28
Kelly Mees <kelly.mees@student.kdg.be>
2019-04-04 13:03:59 +0200

    add get api

--- src/main/java/com/example/demo/controllers/HelloRestController.java
+++ src/main/java/com/example/demo/controllers/HelloRestController.java
@@ -16,4 +16,11 @@ public class HelloRestController {
         return new ResponseEntity(result,HttpStatus.OK);
         //testcomment
     }
+    @GetMapping(value = "/hello/{id}")
+    public ResponseEntity receiveGet(@PathVariable Long id) {
+        System.out.println("in receiveName " + id);
+        String result = "works";
+        return new ResponseEntity(result,HttpStatus.OK);
+        //testcomment
+    }
 }
