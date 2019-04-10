3b0c918105450d712bef5a9ec227ebf2837aa30a
Kelly Mees <kelly.mees@student.kdg.be>
2019-04-10 13:27:22 +0200

    add dialogflowresponse

--- src/main/java/com/example/demo/controllers/HelloRestController.java
+++ src/main/java/com/example/demo/controllers/HelloRestController.java
@@ -1,16 +1,22 @@
 package com.example.demo.controllers;
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import com.google.api.services.dialogflow.v2.model.*;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+import com.google.api.client.json.JsonGenerator;
+import com.google.api.client.json.jackson2.JacksonFactory;
 
 @RestController
 public class HelloRestController {
+    private static JacksonFactory jacksonFactory = JacksonFactory.getDefaultInstance();
 
-    @RequestMapping(value = "/helloWorld/{id}", method= RequestMethod.POST)
+
+  /*  @RequestMapping(value = "/helloWorld/{id}", method= RequestMethod.POST)
     public ResponseEntity<String> receiveName(@PathVariable Long id) {
         System.out.println("in receiveName " + id);
         String result = "works";
@@ -35,5 +41,31 @@ public class HelloRestController {
     public HttpEntity statusChange(@PathVariable String name, @PathVariable String status){
         System.out.println("in statusChange for " + name + " change status to " + status);
         return new HttpEntity(HttpStatus.OK);
+    }*/
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/dialogFlow", method = RequestMethod.POST)
+    public ResponseEntity<String> webhookCall(@RequestBody String msg) throws IOException{
+        System.out.println("in webhookCall");
+        System.out.println(msg);
+        System.out.println("##############################");
+        GoogleCloudDialogflowV2WebhookRequest request = jacksonFactory.createJsonParser(msg)
+                .parse(GoogleCloudDialogflowV2WebhookRequest.class);
+        for(Map.Entry<String,Object> entry:  request.getQueryResult().getOutputContexts().get(0).getParameters().entrySet()){
+            System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
+        }
+
+       StringWriter stringWriter = new StringWriter();
+        JsonGenerator jsonGenerator = jacksonFactory.createJsonGenerator(stringWriter);
+        GoogleCloudDialogflowV2WebhookResponse response = new GoogleCloudDialogflowV2WebhookResponse();
+        response.setFulfillmentText("Het is gelukt!");
+
+        jsonGenerator.serialize(response);
+        jsonGenerator.flush();
+        System.out.println("###############");
+        System.out.println(response);
+
+
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
