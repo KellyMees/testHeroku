c2f7edc81e6c61e0cbe284187588ea458d46a39d
Kelly Mees <kelly.mees@student.kdg.be>
2019-04-11 08:46:35 +0200

    changes

--- src/main/java/com/example/demo/controllers/HelloRestController.java
+++ src/main/java/com/example/demo/controllers/HelloRestController.java
@@ -46,11 +46,18 @@ public class HelloRestController {
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/dialogFlow", method = RequestMethod.POST)
     public ResponseEntity<String> webhookCall(@RequestBody String msg) throws IOException{
+        System.out.println(msg);
         System.out.println("##############################");
         GoogleCloudDialogflowV2WebhookRequest request = jacksonFactory.createJsonParser(msg)
                 .parse(GoogleCloudDialogflowV2WebhookRequest.class);
         for(Map.Entry<String,Object> entry:  request.getQueryResult().getOutputContexts().get(0).getParameters().entrySet()){
             System.out.println("key: " + entry.getKey() + " value: " + entry.getValue());
+        }
+        for(Map.Entry<String,Object> e :request.getOriginalDetectIntentRequest().getPayload().entrySet()){
+            if(e.getKey().equals("user")){
+                System.out.println(e.getValue());
+            }
+           // System.out.println("key: " + e.getKey() + " value: " + e.getValue().toString());
         }
 
        StringWriter stringWriter = new StringWriter();
